omni-compiler/omni-compiler

commit c094d880d32d63d6f7170fe3d65bb5716670086b

fix bug #37

F-FrontEnd/src/F-compile.c
@@ -4188,7 +4188,7 @@ compile_member_ref(expr x)
 
     }
 
-    tp = compile_dimensions(tp, shape);
+    //tp = compile_dimensions(tp, shape);
     fix_array_dimensions(tp);
 
     new_v = expv_cons(F95_MEMBER_REF, tp, struct_v, mX);

omni-compiler/xcodeml-tools-old

commit 0f9e161cde28dcf4e9cb84dad16ef4c275a02c36

fix the degradation caused by the previous commit c094d880d32d63d6f7170fe3d65bb5716670086b

F-FrontEnd/src/F-compile.c
@@ -4183,6 +4183,7 @@ compile_member_ref(expr x)
         }
 
         tp = retTyp;
+	tp = compile_dimensions(tp, shape);
     } else {
         tp = ID_TYPE(member_id);
 